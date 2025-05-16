d5473052e17d747eb449e326a473945817ec69f2
MuleevTagir <granik2007@yandex.ru>
2025-05-16 17:01:47 +0300

    Этап 3. Добавляем рекомендательный сервис

--- core/user-api/src/main/java/ru/practicum/application/user/api/InnerUserInterface.java
+++ core/user-api/src/main/java/ru/practicum/application/user/api/InnerUserInterface.java
@@ -2,6 +2,8 @@ package ru.practicum.application.user.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.practicum.application.api.dto.user.UserDto;
 import ru.practicum.application.api.exception.NotFoundException;
 
